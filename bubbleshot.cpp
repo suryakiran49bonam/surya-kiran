ad1bc0738aa865232cb9a94cbeb82362eed7cc2d
suryakiran49bonam <suryakiranbonam43@gmail.com>
2025-11-27 09:51:12 +0530

    Add files via upload

---
+++ bubbleshot.cpp
@@ -0,0 +1,44 @@
+#include <stdio.h>
+int main()
+{
+	int n,i,a[10],j,temp;
+	printf("enter n value:");
+	scanf("%d",&n);
+	printf("enter array elements");
+	for(i=0;i<n;i++)
+	{
+	
+	scanf("%d",&a[i]);
+    }
+	for(i=0;i<n-1;i++)
+	{
+		for(j=0;j<n-i-1;j++){
+		
+		
+		if(a[j]>a[j+1])
+		{
+			temp=a[j];
+			a[j]=a[j+1];
+			a[j+1]=temp;
+		}
+	   }
+	   printf("the element in sorted: %d\n",a[j]);
+	}
+	return 0;
+	
+
+//	printf("sorted array elements");
+//	for(i=0;i<n;i++){
+	
+	
+//	printf("%d",a[i]);
+//	return 0;
+   
+  // printf("sorted array elements");
+//	for(i=0;i<n;i++){
+	
+	
+//	printf("%d",a[i]);
+//	return 0;
+}
+
