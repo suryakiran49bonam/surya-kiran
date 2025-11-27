bd0ef71fa1708b79c386331eb80811d4c5293f11
suryakiran49bonam <suryakiranbonam43@gmail.com>
2025-11-27 10:54:05 +0530

    Add files via upload

---
+++ sum of digits.cpp
@@ -0,0 +1,17 @@
+#include<stdio.h>
+int main()
+{
+	int num,sum,rem;
+	printf("enter a 5 digit number");
+	scanf("%d",&num);
+	sum = 0;
+	while(num!=0);
+	{
+		rem = num % 10;
+		sum = rem;
+		num = num/10;
+	}
+	printf("sum of digits is %d",sum);
+	return 0;
+	}
+
