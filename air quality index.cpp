be0f426e39a6d8235096965dc8271b69bd0278fa
suryakiran49bonam <suryakiranbonam43@gmail.com>
2025-12-03 13:26:36 +0530

    Add files via upload

---
+++ air quality index.cpp
@@ -0,0 +1,14 @@
+#include <stdio.h>
+
+int main() {
+    int X;
+    scanf("%d", &X);
+
+    if (X < 100) {
+        printf("YES\n");
+    } else {
+        printf("NO\n");
+    }
+
+    return 0;
+}
